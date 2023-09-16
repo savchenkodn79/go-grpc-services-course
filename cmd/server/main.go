80f0204e0e12fe8e788e444a42d54ece22200e86
dima <savchenkodn79@gmail.com>
2023-09-16 11:27:41 +0300

    Added a db and migrate

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,27 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/savchenkodn79/go-grpc-services-course/internal/db"
+	"github.com/savchenkodn79/go-grpc-services-course/internal/rocket"
+)
 
 func Run() error {
 	// responsible for initializing and starting
 	// out gRPC server
+	rocketStore, err := db.New()
+	if err != nil {
+		return err
+	}
+
+	err = rocketStore.Migrate()
+	if err != nil {
+		log.Println("Failed to run migations")
+		return err
+	}
+	_ = rocket.New(rocketStore)
+
 	return nil
 }
 
